5986368f12679bd54cac0ac2da3eeedeba61d52c
Jayu <jayuze@gmail.com>
2025-01-06 17:00:44 +0100

    i think i can push this, but a few more tests tomorrow

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 19:15:52 by juitz             #+#    #+#             */
-/*   Updated: 2024/12/17 19:17:04 by juitz            ###   ########.fr       */
+/*   Updated: 2025/01/06 17:00:10 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 #include <iostream>
 int main( void )
 {
-	Fixed  a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed  a(10);
+	Fixed const b(Fixed( 5.05f ) * Fixed( 2 ));
+	Fixed c(15);
+	
+	if (a > b)
+		std::cout << "a is bigger than b" << std::endl;
+	else if (a < b)
+		std::cout << "b is bigger than a" << std::endl;
+	if (a == b)
+		std::cout << "a and b are equal" << std::endl;
+	else if (a != b)
+		std::cout << "a and b are not equal" << std::endl;
+	
+	std::cout << "a + c: " << a + c << std::endl;
+	std::cout << "a - c: " << a - c << std::endl;
+	std::cout << "c / a: " << c / a << std::endl;
+	std::cout << "c * a: " << c * a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
@@ -23,5 +38,7 @@ int main( void )
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
-	return 0;
+	std::cout << Fixed::min( a, b ) << std::endl;
+	
+	return (0);
 }
